c45f4b289644a6ae16c383e38d5d67ba57a62c5b
PuvithaGanapathi <puvithaganapathi@gmail.com>
2024-09-23 19:53:49 +0530

    Add files via upload

---
+++ portfolio_finalpage/portfolio_final/portfolio (4)/portfolio (4)/portfolio/example.js
@@ -0,0 +1,7 @@
+var typed=new typed(".text",{
+    Strings:["web devloper","full stach developer","frontend developer"],
+    backSpeed:100,
+    typeSpeed:100,
+    backDelay:1000,
+    loop:true
+})
